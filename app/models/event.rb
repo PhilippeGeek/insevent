4292fb8f22c802d08ec0c55dd83032a19434bbd3
Philippe Vienne <philippe@vienne.me>
2016-03-30 19:41:21 +0200

    Add validators for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :tags
+  validates_presence_of :name, :description, :email, :website, :location, :time
+  validates :email, email: true
 end
