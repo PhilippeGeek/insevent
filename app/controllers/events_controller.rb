fb47ebfc19f44b70ef755de48643449380716fad
Philippe Vienne <philippe@vienne.me>
2016-03-30 23:05:17 +0200

    Add the JSON api

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,38 @@
 class EventsController < ApplicationController
   def index
     @events = Event.published
+    respond_to do |format|
+      format.html { render }
+      format.json { render json: @events }
+    end
   end
 
   def show
-    @event = Event.find params[:id].to_i
+    @event = Event.published.find params[:id].to_i
+    respond_to do |format|
+      format.html { render }
+      format.json { render json: @event }
+    end
   end
 
   def new
     @event = Event.new
+    respond_to do |format|
+      format.html { render }
+    end
   end
 
   def create
     @event = Event.new event_params
-    if @event.save
-      redirect_to @event
-    else
-      render :new
+    success = @event.save
+    respond_to do |format|
+      format.html do
+        if success
+          redirect_to @event
+        else
+          render :new
+        end
+      end
     end
   end
 
