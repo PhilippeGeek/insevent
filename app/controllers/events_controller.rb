918150384e5360d800cd728775113dcd005a7644
Philippe Vienne <philippe@vienne.me>
2016-03-30 21:49:31 +0200

    Add minimal code for the controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.published
   end
 
   def show
+    @event = Event.find params[:id].to_i
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new event_params
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :time, :location, :email, :website, tags_ids: [])
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :tags
   validates_presence_of :name, :description, :email, :website, :location, :time
+
+  scope :published, -> () {where(published: true)}
 end
